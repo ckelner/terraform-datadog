46f19f3d343e70733ab990804eeaf760d57ac468
ckelner <chris.kelner@gmail.com>
2018-01-30 15:32:16 -0500

    add outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+#############################################################################
+# Outputs
+# https://www.terraform.io/docs/configuration/outputs.html
+#############################################################################
+output "vpc_id" {
+    value = "${aws_vpc.main.id}"
+}
+output "subnet_ids" {
+    value = ["${aws_subnet.public.*.id}"]
+}
+output "instance_id" {
+    value = "${aws_instance.web.id}"
+}
+output "instance_ip" {
+    value = "${aws_instance.web.public_ip}"
+}
+output "datadog_monitor_id" {
+    value = "${datadog_monitor.foo.id}"
+}
