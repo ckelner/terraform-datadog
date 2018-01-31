81c6f84c30ca63418eeb6fcbda99bd0afa938568
ckelner <chris.kelner@gmail.com>
2018-01-30 19:20:44 -0500

    add node ip to monitor as example

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -5,12 +5,15 @@
 # Note: take note of how we can include terraform values in our monitor's
 # template -- if we wanted a very specific monitor for a specific host, where
 # we likely had some very static resource, we could include things like the ip
-# address for that host easily by invoking `${aws_instance.web.public_ip}`
+# address for that host easily by invoking `${aws_instance.web.public_ip}` - but
+# typically you'd want to leverage datadog to get this information, e.g.
+# `{{host.ip}}` as you see in the second example; this only serves as an example
 resource "datadog_monitor" "common_disk_full" {
   name = "[${var.common_name}] {{host.name}} {{device.name}} disks are full - [${var.monitor_suffix}]"
   type = "metric alert"
   message = <<EOT
-  {{host.name}} disks are full, let's understand why @slack-channel
+  {{host.name}} with ip ${aws_instance.web.public_ip} disk are full, let's
+  understand why @slack-channel
 
   {{#is_alert}}
 
@@ -39,7 +42,7 @@ module "cpu_monitor" {
   source      = "./datadog_metric_monitor_module"
   name        = "[${var.common_name}] {{host.name}} CPU is {{value}} - [${var.monitor_suffix}]"
   message     = <<EOT
-  {{host.name}} CPU is high!
+  {{host.name}} w/ ip {{host.ip}} CPU is high!
   CPU has been above {{threshold}} for the last 5 minutes!
   @pagerduty-Datadog-Demo
 EOT
